e9a5b51522145ccd812fc9d8285a4ff97fb51367
thecoderzone <Sylversipi@yahoo.co.uk>
2022-09-28 23:34:39 +0000

    commit message

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def s_r_elm(elm):
-        return (elm if elm != search else replace)
-    return list(map(s_r_elm, my_list))
+    return [replace if i == search else i for i in my_list]
