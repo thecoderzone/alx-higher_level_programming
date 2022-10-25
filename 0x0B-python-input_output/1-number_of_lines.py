f3378a3dec6c608087ee40019888f5eb38618183
thecoderzone <sylversipi@yahoo.co.uk>
2022-10-25 16:29:36 -0700

    numbers

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_lines function
-"""
+# 1-number_of_lines.py
+# Sylvester Sipitey
+"""Defines a text file line-counting function."""
 
 
 def number_of_lines(filename=""):
-    """returns the number of lines of a text file"""
-    with open(filename, 'r', encoding='utf8') as f:
-        i = 0
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
         for line in f:
-            i += 1
-        return i
+            lines += 1
+    return lines
