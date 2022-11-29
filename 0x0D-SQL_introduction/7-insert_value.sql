2ef5d0e78a156b943d67ca59d1360e4a44f91c0f
thecoderzone <sylversipi@yahoo.co.uk>
2022-11-29 15:55:39 -0800

    superfix

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- Inserts a new row in the table `first_table`
--- in database `hbtn_0c_0` in MySQL Server.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School")
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
